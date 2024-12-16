46b1aaec2a6c01475c4315ddeafa92e4e5c50d63
slaystarthunder <72455891+slaystarthunder@users.noreply.github.com>
2024-12-16 21:27:25 +0100

    Initial commit

---
+++ src/test/java/org/example/PrimeNumberCalculatorTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PrimeNumberCalculatorTest {
+
+        @Test
+        public void isPrime_supposedToBeTrueForPrimeNumbers() {
+            PrimeNumberCalculator calculator = new PrimeNumberCalculator();
+
+            // Tests if isPrime returns one of the 4 prime numbers between 0 and 10
+            assertTrue(calculator.isPrime(2));
+            assertTrue(calculator.isPrime(3));
+            assertTrue(calculator.isPrime(5));
+            assertTrue(calculator.isPrime(7));
+
+        }
+}
